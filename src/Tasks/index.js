8288390744d099cbe2150e5cfb7c5d5c1cd08439
Grzegorz Domin <grze.domin98@gmail.com>
2022-02-05 17:32:04 +0100

    function Tasks destructured

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
 
-const Tasks = (props) => (
+const Tasks = ({tasks, hideDoneTasks}) => (
     <ul className="tasks">
-        {props.tasks.map(task => (
-            <li key={task.id} className={`main__listItem${task.done && props.hideDoneTasks ? " main__listItem--hidden" : ""}`}>
+        {tasks.map(task => (
+            <li key={task.id} className={`main__listItem${task.done && hideDoneTasks ? " main__listItem--hidden" : ""}`}>
                 <button className="main__taskDoneButton">{task.done ? "✓" : ""}</button>
                    <span className={`main__taskListContent${task.done ? " main__listItem--done" : ""}`}>{task.content}</span>
                 <button className="main__taskRemoveButton">🗑</button>
