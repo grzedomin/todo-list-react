3b9d36a569471c62fa72bf48faca1b5e14dfe08b
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-08 12:03:55 +0200

    tasks imported as an array

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button, ButtonsContainer } from "../StyledButtons/styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasksState, toggleHideDone, setAllDone, selectHideDone, selectTasksEmpty } from "../../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone, selectHideDone, selectTasksEmpty } from "../../tasksSlice";
 
 const Buttons = () => {
-    const { tasks } = useSelector(selectTasksState);
+    const tasks  = useSelector(selectTasks);
     const hideDone = useSelector(selectHideDone);
     const areTasksEmpty = useSelector(selectTasksEmpty);
     const dispatch = useDispatch();
