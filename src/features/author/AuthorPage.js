b515892a141755ac83623be82765aea1cb7b8adf
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-13 20:48:30 +0200

    changed author description

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -7,7 +7,7 @@ export default () => (
         <Header title="O autorze" />
         <Section
             title="Grzegorz Domin"
-            body={<p>Hej! Nazywam się <strong>Grzegorz</strong> i mam <strong>23</strong> lata. Programuję od pół roku i idzie mi coraz lepiej! Moją pasją jest muzyka, od 7 lat gram na <strong>perkusji</strong>, od prawie dwoch na <strong>gitarze</strong>. Programowania uczę się od pół roku i z dnia na dzień wychodzi mi to coraz lepiej ;)
+            body={<p>Hej! Nazywam się <strong>Grzegorz</strong> i mam <strong>23</strong> lata. Programuję od pół roku i każdego dnia stawiam kolejny krok aby zostać Front-End Developerem. Moją pasją jest muzyka, od 7 lat gram na <strong>perkusji</strong>, od prawie dwóch na <strong>gitarze</strong>. Oprócz muzyki uwielbiam sport, piłka nożna, squash to dwie aktywności, którę lubię najbardziej ;)
             </p>}
         />
     </Container>
