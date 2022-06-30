80b1a483269edb7aab6441a1363d5ba846b58b85
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-30 10:56:21 +0200

    url corrected

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampasks.json");
+    const response = await fetch("/todo-list-react/exampleTasks.json");
 
     if(!response.ok) {
         throw new Error(response.statusText);
